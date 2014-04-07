4e248e90023e26d18a95b94676dbe679110c8f75
TCattitude <TCattitude@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-04-07 19:34:25 +0000

    Tagging version 1.0.5.1
    git-svn-id: https://plugins.svn.wordpress.org/vidsy/tags/1.0.5.1@888927 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- vidsy.php
+++ vidsy.php
@@ -3,7 +3,7 @@
 Plugin Name: Vidsy.tv
 Plugin URI: https://wordpress.org/plugins/vidsy/
 Description: WordPress integration with Vidsy.tv
-Version: 1.0.5.0
+Version: 1.0.5.1
 Author: Vidsy.tv
 Author URI: http://vidsy.tv
 License: GPL2+
